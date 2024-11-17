a11ed0fb6d859fad5ea56d27fcad04533b8cd909
Aniudupa15 <aniudupa15@gmail.com>
2024-11-17 17:51:28 +0530

    CHANGES IN SPLASH SCREEN

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:judica/HomePage.dart';
+import 'package:judica/slpash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const Homepage(),
+      home: const SplashScreen(),
     );
 }
 }
